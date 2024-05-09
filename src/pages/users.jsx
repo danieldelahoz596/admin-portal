b12fdd12f12a59f9df4e5cbe0461ab8afa23f9b9
danieldelahoz596 <danieldelahoz596@gmail.com>
2024-05-09 03:41:23 -0500

    show approval and reject buttons

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -1,4 +1,4 @@
-import { Button, Table } from "antd";
+import { Button, Table, Space, Image } from "antd";
 import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
@@ -17,19 +17,73 @@ const columns = [
     key: "email",
   },
   {
-    title: "Phone Area Code",
-    dataIndex: "phone_area_code",
-    key: "phone_area_code",
-  },
-  {
-    title: "Phone Number",
-    dataIndex: "phone_number",
-    key: "phone_number",
-  },
-  {
     title: "Role",
-    dataIndex: "role",
-    key: ["role", "role_name"],
+    dataIndex: ["role", "role_name"],
+    key: "role",
+  },
+  {
+    title: "Photo Front",
+    dataIndex: "photo_front",
+    key: ["userVerification", "photo_front"],
+    render: (_, record) => (
+      <Image
+        width={200}
+        src={record.userVerification && record.userVerification.photo_front}
+        placeholder={
+          <Image
+            preview={false}
+            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png?x-oss-process=image/blur,r_50,s_50/quality,q_1/resize,m_mfit,h_200,w_200"
+            width={200}
+          />
+        }
+      />
+    ),
+  },
+  {
+    title: "Photo Back",
+    dataIndex: "photo_back",
+    key: ["userVerification", "photo_back"],
+    render: (_, record) => (
+      <Image
+        width={200}
+        src={record.userVerification && record.userVerification.photo_back}
+        placeholder={
+          <Image
+            preview={false}
+            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png?x-oss-process=image/blur,r_50,s_50/quality,q_1/resize,m_mfit,h_200,w_200"
+            width={200}
+          />
+        }
+      />
+    ),
+  },
+  {
+    title: "Photo",
+    dataIndex: "selfie_photo",
+    key: ["userVerification", "selfie_photo"],
+    render: (_, record) => (
+      <Image
+        width={200}
+        src={record.userVerification && record.userVerification.selfie_photo}
+        placeholder={
+          <Image
+            preview={false}
+            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png?x-oss-process=image/blur,r_50,s_50/quality,q_1/resize,m_mfit,h_200,w_200"
+            width={200}
+          />
+        }
+      />
+    ),
+  },
+  {
+    title: "Action",
+    key: "action",
+    render: (_, record) => (
+      <Space size="middle">
+        <Button>Approval</Button>
+        <Button>Reject</Button>
+      </Space>
+    ),
   },
 ];
 
@@ -49,7 +103,9 @@ const UserPage = () => {
     httpClient.get("/users").then((response) => {
       console.log("response", response.data.data);
       setUserList(
-        response.data.data.map((item) => ({ key: item.id, ...item }))
+        response.data.data
+          .filter((item) => item.selling_request)
+          .map((item) => ({ key: item.id, ...item }))
       );
       setIsLoading(false);
     });
@@ -60,7 +116,7 @@ const UserPage = () => {
   };
 
   const handleAddNewUser = (user) => {
-    setUserList([...userList, user]);
+    setUserList([...userList, { ...user, key: user.id }]);
   };
 
   if (isLoading) {

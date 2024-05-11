a96d156564f5652d274960893ab3e0dbbcfc6821
danieldelahoz596 <danieldelahoz596@gmail.com>
2024-05-10 19:42:59 -0500

    update business settings

--- src/pages/businessSettings.jsx
+++ src/pages/businessSettings.jsx
@@ -1,15 +1,17 @@
-import { Form } from "antd";
+import { Form, Input, Button, message } from "antd";
 import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import httpClient from "../utils/httpClient";
 
 const BusinessSettingsPage = () => {
+  const [messageApi, contextHolder] = message.useMessage();
   const { user } = useAuth();
   const navigate = useNavigate();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [businessSetting, setBusinessSetting] = useState(null);
+  const [form] = Form.useForm();
 
   useEffect(() => {
     if (user && user.role_id !== 1) navigate("/");
@@ -18,11 +20,20 @@ const BusinessSettingsPage = () => {
   useEffect(() => {
     setIsLoading(true);
     httpClient.get("/business-settings").then((response) => {
-      console.log("response", response.data.data);
+      form.setFieldsValue(response.data.data);
       setBusinessSetting(response.data.data);
       setIsLoading(false);
     });
   }, []);
+
+  const handleSave = () => {
+    form.validateFields().then((values) => {
+      httpClient.post("/business-settings", values).then((response) => {
+        form.setFieldsValue(response.data.data);
+        message.success("Succcess to save bussiness settings");
+      });
+    });
+  };
 
   if (isLoading) {
     return (
@@ -34,26 +45,40 @@ const BusinessSettingsPage = () => {
 
   return (
     <>
-      <Form labelCol={{ span: 8 }} labelAlign="left">
-        <Form.Item label="Selling Fee Percentage">
-          {businessSetting?.selling_fee_percentage}
+      {contextHolder}
+      <Form labelCol={{ span: 8 }} labelAlign="left" form={form}>
+        <Form.Item label="Selling Fee Percentage" name="selling_fee_percentage">
+          <Input />
         </Form.Item>
-        <Form.Item label="Selling Fee Minimum">
-          {businessSetting?.selling_fee_minimum}
+        <Form.Item label="Selling Fee Minimum" name="selling_fee_minimum">
+          <Input />
         </Form.Item>
-        <Form.Item label="Shipping Fee Percentage">
-          {businessSetting?.shipping_fee_percentage}
+        <Form.Item
+          label="Shipping Fee Percentage"
+          name="shipping_fee_percentage"
+        >
+          <Input />
         </Form.Item>
-        <Form.Item label="Shipping Fee Minimum">
-          {businessSetting?.shipping_fee_minimum}
+        <Form.Item label="Shipping Fee Minimum" name="shipping_fee_minimum">
+          <Input />
         </Form.Item>
-        <Form.Item label="Transaction Fee Percentage">
-          {businessSetting?.transaction_fee_percentage}
+        <Form.Item
+          label="Transaction Fee Percentage"
+          name="transaction_fee_percentage"
+        >
+          <Input />
         </Form.Item>
-        <Form.Item label="Transaction Fee Minimum">
-          {businessSetting?.transaction_fee_minimum}
+        <Form.Item
+          label="Transaction Fee Minimum"
+          name="transaction_fee_minimum"
+        >
+          <Input />
         </Form.Item>
-        <Form.Item label="VAT Rate">{businessSetting?.vat_rate}</Form.Item>
+        <Form.Item label="VAT Rate" name="vat_rate">
+          <Input />
+        </Form.Item>
+
+        <Button onClick={handleSave}>Save</Button>
       </Form>
     </>
   );
